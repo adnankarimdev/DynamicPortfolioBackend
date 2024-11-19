24311e584cb2d13d4d6b47e834f365b717b7af88
Adnan Karim <adnan.karim.dev@gmail.com>
2024-11-18 19:06:13 -0800

    app started for backend

---
+++ backend/views.py
@@ -0,0 +1,56 @@
+from django.http import HttpResponse
+from django.http import HttpResponse
+import os
+from dotenv import load_dotenv
+from collections import defaultdict
+from django.views.decorators.http import require_http_methods
+import time
+import re
+import json
+import hashlib
+from collections import Counter
+from django.db.models import Q
+from django.utils import timezone
+from django.forms.models import model_to_dict
+from django.core import serializers
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from langchain_openai import ChatOpenAI
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+import pickle
+
+# from langchain.vectorstores import FAISS
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from rest_framework import status
+from rest_framework.response import Response
+import jwt
+import secrets
+import googlemaps
+import requests
+from django.conf import settings
+from icalendar import Calendar, Event
+from datetime import datetime, timedelta
+from token_count import TokenCount
+from datetime import datetime
+import pytz
+from twilio.rest import Client
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=1,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
--- webapp/settings.py
+++ webapp/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+os.environ["OPENAI_API_KEY"] = (
+    "sk-proj-BkqMCfMCu8aJz0M19aj9T3BlbkFJCqFGN85AiM1NP2lJyrF1"
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
